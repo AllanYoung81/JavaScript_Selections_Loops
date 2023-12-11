5943e3c44e853ac1b6f840eaf944af9f1ab0f6bc
Allan Young <ayounggtr@gmail.com>
2023-12-11 11:49:49 -0500

    JSLoops Redone

--- app.js
+++ app.js
@@ -13,41 +13,50 @@ for (let i = 0; i < 100; i++){
 
 // Exercise 2 FIZZBUZZ
 for (let i = 0; i < 100; i++){
-    if (i % 3 == 0 && i % 5 == 0){
+    console.log(i);
+
+    if (i % 3 == 0 && i % 5 == 0) {
         console.log(i,"FIZZBUZZ");
-    }else if (i % 3 == 0){
+
+    } else { 
+        if (i % 3 == 0) {
         console.log(i,"FIZZ)");
-     }else if(i % 3 == 0){
+        }
+      if(i % 3 == 0) {
         console.log(i,"(BUZZ)");
     }
-    console.log(i);
+    
 
 }
 
+}
 
 
 // Exercise 3 
 
 let i = 1;
 
-while (i = 100){
+while (i < 100){
    if (i % 2 != 0) {
     console.log (i);
    }
+
    i++;
 }
 
+let x = 1;
+
 do{
-    if (x % 2 != 0){
+    if (x % 2 !== 0){
         console.log(x);
     }
     x++;
 
-} while (x < 100)
+} while (x < 100);
 
 // Exercise 4
-let numberToFind = Math.round(Math.random()* 500);
-let n = Math.round(Math.random()* (500 - 100)+100);
+let numberToFind = Math.round(Math.random() * 500);
+let n = Math.round(Math.random() * (500 - 100) +100);
 
 for (let i = 1; i <= n; i++){
     if (i == numberToFind){
@@ -55,8 +64,9 @@ for (let i = 1; i <= n; i++){
     console.log(`Found ${numberToFind}`);
     break;
 }
-
-console.log(`Did not find ${numberToFind}`);
+if(i ==n){
+console.log(`Did not find ${numberToFind} within the scope/range.`);
+}
 }
 
 
